57ef07240b33559510f1b0c6338fac314364b08e
Brandon589 <47423841+Brandon589@users.noreply.github.com>
2020-04-22 17:32:31 -0400

    Create Month_To_Day.py

---
+++ Month_To_Day.py
@@ -0,0 +1,27 @@
+month = input("Enter Month: ")
+if month == "Febuary":
+    print("28 or 29 days")
+elif month == "January":
+    print("31 days")
+elif month == "March":
+    print("31 days")
+elif month == "April":
+    print("30 days")
+elif month == "May":
+    print("31 days")
+elif month == "June":
+    print("30 days")
+elif month == "July":
+    print("31 days")
+elif month == "August":
+    print("31 days")
+elif month == "September":
+    print("30 days")
+elif month == "October":
+    print("31 days")
+elif month == "November":
+    print("30 days")
+elif month == "December":
+    print("31 days")
+else:
+    print("Invalid response")
